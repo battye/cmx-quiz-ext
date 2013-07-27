f5ae9ab3e49261d5dc100e59ec068392d70407fb
battye <battye@phpbb.com>
2013-07-28 01:06:58 +0800

    Add UMIL for quiz installation.

---
+++ root/language/en/mods/info_acp_quiz.php
@@ -0,0 +1,18 @@
+<?php
+// Ultimate Quiz MOD - Module language file
+
+if( !defined('IN_PHPBB') )
+{
+	exit;
+}
+
+if( empty($lang) || !is_array($lang) )
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	// Admin
+	'ACP_UQM_QUIZ'				=> 'Quiz',
+));
+?>
